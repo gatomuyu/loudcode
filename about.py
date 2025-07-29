195da0fd4eaef5bece4c3ef15ca71d2ecbe97d1f
gatomuyu <gatomuyuwang@gmail.com>
2025-07-29 09:05:57 +0200

    Create about.py

---
+++ about.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Learn More",
+    page_icon = "📄")
+
+st.title("About")
+st.write("**This is A Great Way To Join A Python Job And So We Thank You With Your Support Because Of You :)**")
+st.write("**(This Web Is Still Under Construction So DONT Expect Anything To Work)**")
+st.write("**In Reality, Im The Only One Working On This Website**")
+st.write("**This Project Is Using Streamlit, So I Am Very Limited To Whatever I Can Do**")
+st.header("**How Does The Website Work?**")
+st.write("**When You Open The Page, You Are Sent To The Home Page**")
+st.write("**There Are Four Tabs On The Right [Home Page, Register, Learn More, Settings]**")
+st.header("**Home Page**")
+st.write("The Home Page is the Main Page, In Which You Start At, Theres Not Much To Do There But A Couple Things")
+st.header("**Register Page**")
+st.write("**The Register Page Is The Page That You Create An Account, So You Must Answer A Couple Questions**")
+st.write("**The First Three Questions Is To Set up An Account, Put A Username (Dont Put Your Real Name), Make A Password And Then Confirm your Password**")
+st.write("**Most Of The Other Questions Are Up To You**")
+st.write("**But There is An Other Questions That Ask's For Your App Password**")
+st.header("**What Is An App Password?**")
